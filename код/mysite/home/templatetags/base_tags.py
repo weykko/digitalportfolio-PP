3e73f4ba01c1bde758f7986ed61bd05f4d4c9faf
g00sdu <g00sdu@gmail.com>
2024-04-16 22:35:14 +0500

    Создано рабочее меню между вкладками

--- код/mysite/home/templatetags/base_tags.py
+++ код/mysite/home/templatetags/base_tags.py
@@ -1,5 +1,6 @@
 from home.models import Footer
 from django import template
+from wagtail.models import Site
 
 register = template.Library()
 
@@ -10,4 +11,17 @@ def footer_tag(context):
     return {
         'request': context['request'],
         'footer': Footer.objects.first()
+    }
+@register.simple_tag(takes_context = True)
+def get_site_root(context):
+    return Site.find_for_request(request=context['request']).root_page
+
+@register.inclusion_tag('tags/top_menu.html', takes_context=True)
+def top_menu(context, parent):
+    menuitems = parent.get_children().live().in_menu()
+
+    return {
+        'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
     }
